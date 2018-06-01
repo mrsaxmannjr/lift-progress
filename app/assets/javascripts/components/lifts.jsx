c3bd5a00296f7991210e9137e1bd892f1ae7c626
mrsaxmannjr <mrsaxmannjr@gmail.com>
2018-06-01 00:20:01 -0600

    added metric toggle and coefficient conversions

--- app/assets/javascripts/components/lifts.jsx
+++ app/assets/javascripts/components/lifts.jsx
@@ -5,15 +5,44 @@ class Lifts extends React.Component {
       lifts: props.data,
       date: "",
       liftname: "",
-      ismetric: "",
+      ismetric: false,
       weightlifted: "",
       repsperformed: "",
-      onerm: ""
+      onerm: 0,
+      coefficients: {
+        1: 1,
+        2: .943,
+        3: .906,
+        4: .881,
+        5: .851,
+        6: .831,
+        7: .807,
+        8: .786,
+        9: .765,
+        10: .744
+      }
     };
     console.log(this.state.lifts);
     this.handleChange = this.handleChange.bind(this)
     this.handleSubmit = this.handleSubmit.bind(this)
     this.getFormData = this.getFormData.bind(this)
+    this.postNewLift = this.postNewLift.bind(this)
+    this.toggleUnit = this.toggleUnit.bind(this)
+    this.calculateOneRM = this.calculateOneRM.bind(this)
+  }
+
+  toggleUnit(event) {
+    event.preventDefault();
+    this.setState({
+      ismetric: !this.state.ismetric
+    })
+  }
+
+  calculateOneRM() {
+    const weightlifted = this.state.weightlifted
+    const repsperformed = this.state.repsperformed
+
+    weightlifted && repsperformed ? this.state.onerm = weightlifted / this.state.coefficients[repsperformed] : 0
   }
 
   handleChange(event) {
@@ -35,8 +64,25 @@ class Lifts extends React.Component {
       onerm: this.state.onerm
     }
     this.getFormData(formData)
+    this.postNewLift(formData)
     console.log("form: ", formData);
+  }
 
+  getFormData (formData) {
+    const newFormData = this.state.lifts
+    newFormData.push(formData)
+    this.setState({
+      lifts: newFormData,
+      // date: "",
+      // liftname: "",
+      // ismetric: "",
+      // weightlifted: "",
+      // repsperformed: "",
+      // onerm: ""
+    })
+  }
+
+  postNewLift(formData) {
     fetch("", {
       method: "POST",
       headers: {
@@ -47,19 +93,6 @@ class Lifts extends React.Component {
       .then(liftData => console.log("liftData: ", liftData))
   }
 
-  getFormData (formData) {
-    const newFormData = this.state.lifts
-    newFormData.push(formData)
-    this.setState({
-      lifts: newFormData,
-      date: "",
-      liftname: "",
-      ismetric: "",
-      weightlifted: "",
-      repsperformed: "",
-      onerm: ""
-    })
-  }
   render () {
     return (
       <div className="lifts">
@@ -67,19 +100,34 @@ class Lifts extends React.Component {
         <form className="form-inline" onSubmit={this.handleSubmit}>
           <div className="form-group">
             <input type="date" className="form-control" placeholder="date" name="date" value={this.state.value} onChange={this.handleChange} />
+
             <input type="text" className="form-control" placeholder="liftname" name="liftname" value={this.state.value} onChange={this.handleChange} />
-            <input type="boolean" className="form-control" placeholder="ismetric" name="ismetric" value={this.state.value} onChange={this.handleChange} />
+
+            <a className="btn btn-primary" onClick={this.toggleUnit}>Metric = {this.state.ismetric.toString()}</a>
+
             <input type="number" className="form-control" placeholder="weightlifted" name="weightlifted" value={this.state.value} onChange={this.handleChange} />
-            <input type="number" className="form-control" placeholder="repsperformed" name="repsperformed" value={this.state.value} onChange={this.handleChange} />
-            <input type="number" className="form-control" placeholder="onerm" name="onerm" value={this.state.value} onChange={this.handleChange} />
+
+            <input type="number" className="form-control" placeholder="repsperformed" name="repsperformed" min="1" max="10" value={this.state.value} onChange={this.handleChange} />
+
+            {/* <input type="number" className="form-control" placeholder="onerm" name="onerm" value={this.state.value} onChange={this.handleChange} /> */}
+
             <button type="submit" className="btm btn-primary">Create Lift</button>
+
+            <div className="card">
+              <div className="card-block">
+                <h2 className="card-title text-xs-center">1 RM Estimate</h2>
+                <h3 className="card-title text-xs-center">{this.calculateOneRM()}</h3>
+              </div>
+            </div>
           </div>
         </form>
+
         <table className="table table-bordered">
           <thead></thead>
           <th>Date</th>
           <th>Lift Name</th>
           <th>Weight Lifted</th>
+          <th>Metric ?</th>
           <th>Reps Performed</th>
           <th>1 Rep Max</th>
           <tbody>{this.state.lifts.map(lift => {
@@ -88,6 +136,7 @@ class Lifts extends React.Component {
                 <td>{lift.date}</td>
                 <td>{lift.liftname}</td>
                 <td>{lift.weightlifted}</td>
+                <td>{lift.ismetric.toString()}</td>
                 <td>{lift.repsperformed}</td>
                 <td>{lift.onerm}</td>
               </tr>
